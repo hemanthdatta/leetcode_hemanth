0a86a676ad882229b7c0803615c70d74ff9a047f
Nakka Hemanth Datta <146538870+hemanthdatta@users.noreply.github.com>
2025-06-17 15:43:29 +0530

    Time: 2 ms (53.47%), Space: 18.8 MB (62.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #we are using universal hashmap here
-        hashmap={}
+        a=[]
+        h={}
         for i in range(len(nums)):
-            hashmap[nums[i]]=i
+            h[nums[i]]=i
         for i in range(len(nums)):
-            complement=target-nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i,hashmap[complement]]
+            y=target-nums[i]
+            if y in h and h[y]!=i:
+                return [i,h[y]]
+
+
+
         
-        return []
