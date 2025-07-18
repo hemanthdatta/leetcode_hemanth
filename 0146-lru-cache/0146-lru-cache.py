da77bec36931745fdc0d29c77338efab4a47443a
Nakka Hemanth Datta <146538870+hemanthdatta@users.noreply.github.com>
2025-07-18 16:55:40 +0530

    Time: 110 ms (86.3%), Space: 78.1 MB (81.2%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,49 +1,29 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap=capacity
-        self.cache={}
-
-        #left-lru,right=most recent
-        self.left,self.right=Node(0,0),Node(0,0)
-        self.left.next,self.right.prev=self.right,self.left
-
-    def remove(self,node):
-        #it is double linked list so
-        prev,nxt=node.prev,node.next
-        prev.next,nxt.prev=nxt,prev
-    
-    def insert(self,node):
-        prev,nxt=self.right.prev,self.right
-        prev.next=nxt.prev=node
-        node.next,node.prev=nxt,prev        
+        self.cache = OrderedDict()  # This will maintain the order of keys.
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+            # Move the accessed item to the end to mark it as recently used.
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1  # Return -1 if the key doesn't exist.
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key]=Node(key,value)
-        self.insert(self.cache[key])
-        if len(self.cache)>self.cap:
-            #remove from the list and delete the LRU from hashmap
-            lru=self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-        
+            # Update the value of the existing key and move it to the end.
+            self.cache.move_to_end(key)
+        self.cache[key] = value
 
+        # If the cache exceeds the capacity, remove the least recently used item (first item).
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
 
-# Your LRUCache object will be instantiated and called as such:
+# Example usage:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
-# obj.put(key,value)
+# obj.put(key, value)
