f3c53be5d05a4436e180bb5bc318686317e5cb63
Nakka Hemanth Datta <146538870+hemanthdatta@users.noreply.github.com>
2025-07-19 19:16:01 +0530

    Time: 1597 ms (7.06%), Space: 309.7 MB (14.06%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -2,19 +2,18 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp={}
         m,n=len(text1),len(text2)
-        @lru_cache(maxsize=None)
-        def solve(text1,text2,m,n):
+        def solve(m,n):
             if m==0 or n==0:
                 return 0
             elif (m,n) in dp:
                 return dp[(m,n)]
             else:
                 if text1[m-1]==text2[n-1]:
-                    c=1+solve(text1,text2,m-1,n-1)
+                    c=1+solve(m-1,n-1)
                 else:
-                    c1=solve(text1,text2,m-1,n)
-                    c2=solve(text1,text2,m,n-1)
+                    c1=solve(m-1,n)
+                    c2=solve(m,n-1)
                     c=max(c1,c2)
                 dp[(m,n)]=c
             return dp[(m,n)]
-        return solve(text1,text2,m,n)
+        return solve(m,n)
